5d532a6ecce6d5cea92320469b172dc0c0a364dd
roofash <90003109+roofash@users.noreply.github.com>
2021-09-26 11:43:58 -0400

    Add files via upload

---
+++ Boost.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boost : MonoBehaviour
+{
+
+	public float normalspeed;
+	public float boostspeed;
+
+	void Start()
+	{
+		normalspeed = 50f;
+		boostspeed = 100f;
+	}
+
+	void Update()
+	{
+		transform.Translate(normalspeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, normalspeed * Input.GetAxis("Vertical") * Time.deltaTime);
+
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			normalspeed = 100f;
+		}
+        else
+        {
+			normalspeed = 50f;
+        }
+		if (Input.GetKeyUp(KeyCode.LeftShift))
+		{
+			normalspeed = 50f;
+		}
+	}
+}
---
+++ MouseLook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        
+
+        
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
+    
